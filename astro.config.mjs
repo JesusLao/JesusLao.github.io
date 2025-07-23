256952064a981ed8174a6474df81b2d5b4cb8003
JesusLao <yisus.00@hotmail.com>
2025-07-23 19:01:54 +0200

    base removed from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://JesusLao.github.io',
-  base: 'JesusLao.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
